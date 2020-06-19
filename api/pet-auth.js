4abd597e6dc055e07cf3114408dc648f2acaaf5c
Felix Meyer <felixworksdev@gmail.com>
2020-06-18 21:06:33 -0500

    vercel serverless testing

---
+++ api/pet-auth.js
@@ -0,0 +1,27 @@
+let requestBody = {
+  grant_type: "client_credentials",
+  client_id: process.env.API_KEY,
+  client_secret: process.env.API_SECRET,
+};
+
+let bearerToken = "";
+
+const fetchToken = async () => {
+  const result = await fetch(`https://api.petfinder.com/v2/oauth2/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      bearerToken = response.access_token;
+    });
+};
+fetchToken();
+
+module.exports = (req, res) => {
+  res.status(200).send(bearerToken);
+};
