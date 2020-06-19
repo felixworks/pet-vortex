48fcfa5f1c7755d67187089c0a26a93695027b42
Felix Meyer <felixworksdev@gmail.com>
2020-06-18 20:40:02 -0500

    more api testing

--- src/config.js
+++ src/config.js
@@ -1,3 +1,28 @@
+let requestBody = {
+  grant_type: "client_credentials",
+  client_id: process.env.API_KEY,
+  client_secret: process.env.API_SECRET,
+};
+
+let bearerToken = "";
+
+const fetchToken = async () => {
+  const result = await fetch(`https://api.petfinder.com/v2/oauth2/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      bearerToken = response.access_token;
+    });
+};
+fetchToken();
+
 module.exports = {
   API_BASE_URL: "https://api.petfinder.com/v2/animals/",
+  bearerToken: bearerToken,
 };
