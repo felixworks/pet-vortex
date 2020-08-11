8f80d39c77c8d50604a445e37907d17e3cc57542
Felix Meyer <felixworksdev@gmail.com>
2020-08-10 21:44:14 -0500

    refactor by creating List component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { API_BASE_URL } from "./config.js";
+import List from "./List/List.js"
 
 function App() {
-  const [petInfo, setPetInfo] = useState({ pets: [] });
-  const [petParams, setPetParams] = useState({
-    type: "Dog",
-    breed: "Affenpinscher"
-  });
-
-  useEffect(() => {
-    const fetchPets = async () => {
-      const result = await fetch(
-        // TODO add petParams here and add the type and breed function to API
-        `${API_BASE_URL}`,
-        {
-          method: "GET",
-        }
-      )
-        .then(response => response.json())
-        .then(response => setPetInfo({ pets: response.animals }));
-    };
-    fetchPets();
-  }, []);
-
-  // TODO create Tinder-style cards for each dog, and arrange them in a circle that aligns with the viewer. So one card is in focus, and you can see the next 1-2 cards on each side (like a roulette wheel)
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1> Pet Vortex</h1>
       </header>
       <main>
-        <ul>
-          {petInfo
-            ? petInfo.pets.map(item => (
-                <li key={item.id}>
-                  <a href={item.url}>{item.name}<img src={item.photos[0].small} alt={"Dog photo " + item.id}/></a>
-                </li>
-              ))
-            : ""}
-        </ul>
+        <List />
       </main>
     </div>
   );
---
+++ src/List/List.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { API_BASE_URL } from "../config.js";
+
+function List() {
+
+const [petInfo, setPetInfo] = useState({ pets: [] });
+  const [petParams, setPetParams] = useState({
+    type: "Dog",
+    breed: "Affenpinscher"
+  });
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      const result = await fetch(
+        // TODO add petParams here and add the type and breed function to API
+        `${API_BASE_URL}`,
+        {
+          method: "GET",
+        }
+      )
+        .then(response => response.json())
+        .then(response => setPetInfo({ pets: response.animals }))
+    };
+    fetchPets();
+  }, []);
+
+  // TODO create Tinder-style cards for each dog, and arrange them in a circle that aligns with the viewer. So one card is in focus, and you can see the next 1-2 cards on each side (like a roulette wheel)
+return (
+    <ul>
+          {petInfo
+            ? petInfo.pets.map(item => (
+                <li key={item.id}>
+                  <a href={item.url}>{item.name}<img src={item.photos[0] ? item.photos[0].small :  ""} alt={"Dog photo " + item.id}/></a>
+                </li>
+              ))
+            : ""}
+        </ul>
+)
+
+}
+
+export default List;
